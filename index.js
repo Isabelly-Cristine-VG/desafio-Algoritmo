62c9b11f441b33181ae67e3f932b41c2121bf1d3
LauraBuzzato <laura.buzzato@sptech.school>
2025-06-05 12:29:29 -0300

    arrumando logica index

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 let jogadores = [];
+let contador = 0
 
 function adicionarJogador() {
     var nomeIpt = ipt_nome.value.trim();
     var mensagem = '';
+    if (contador < 3) {
+        // Verifica se o nome já está cadastrado
+        if (jogadores.some(j => j.nome === nomeIpt)) {
+            alert("Jogador já cadastrado");
+            return;
+        }
 
-    // Verifica se o nome já está cadastrado
-    if (jogadores.some(j => j.nome === nomeIpt)) {
-        alert("Jogador já cadastrado");
-        return;
+        // Adiciona o novo jogador como objeto
+        jogadores.push({ nome: nomeIpt, dificuldade: null });
+        contador++
+    }
+    else{
+        alert('Só podem ser cadastrados 3 jogadores!')
     }
 
-    // Adiciona o novo jogador como objeto
-    jogadores.push({ nome: nomeIpt, dificuldade: null });
 
     // Monta a mensagem com os jogadores cadastrados
     for (var i = 0; i < jogadores.length; i++) {
@@ -30,4 +37,5 @@ function iniciar() {
     }
 
     console.log(jogadores);
+    window.location.href = "jogo.html"
 }
