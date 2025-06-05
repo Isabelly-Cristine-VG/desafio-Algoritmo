ab40f077701941650d7a1da533bb244389457af1
Igor-Cardoso-SPTECH <igor.trindade@sptech.school>
2025-06-05 11:57:25 -0300

    dificuldade e validação jogadores

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-
-jogadores = []
+let jogadores = [];
 
 function adicionarJogador() {
     var nomeIpt = ipt_nome.value.trim();
     var mensagem = '';
 
     // Verifica se o nome já está cadastrado
-    if (jogadores.includes(nomeIpt)) {
-        alert("Jogador Já Cadastrado");
+    if (jogadores.some(j => j.nome === nomeIpt)) {
+        alert("Jogador já cadastrado");
         return;
     }
 
-    // Adiciona o novo jogador
-    jogadores.push(nomeIpt);
+    // Adiciona o novo jogador como objeto
+    jogadores.push({ nome: nomeIpt, dificuldade: null });
 
     // Monta a mensagem com os jogadores cadastrados
     for (var i = 0; i < jogadores.length; i++) {
-        mensagem += `${i + 1}° Jogador: ${jogadores[i]}<br>`;
+        mensagem += `${i + 1}° Jogador: ${jogadores[i].nome}<br>`;
     }
 
     jogadoresCadastrados.innerHTML = mensagem;
+}
+
+function iniciar() {
+    var dificuldade = ipt_dificuldade.value;
+
+    // Atribui a dificuldade a todos os jogadores
+    for (let i = 0; i < jogadores.length; i++) {
+        jogadores[i].dificuldade = dificuldade;
+    }
+
+    console.log(jogadores);
 }
